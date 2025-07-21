ddfc15412b432eedffe24836217bae232f61c54b
Rahul Rudra <rahulrudra665@gmail.com>
2025-07-21 14:10:39 +0600

    changes made

--- UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/JS Fundementals-02/introtoobjects.js
+++ UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/JS Fundementals-02/introtoobjects.js
@@ -1,7 +1,29 @@
-const jonas = {
-	firstName: "Jonas",
-	lastName: "Schmen",
-	age: 2025 - 1991,
-	job: "teacher",
-	friends: ["Michael", "Peter", "Steven"],
-};
+'use strict';
+
+const jonas = [
+  'Jonas',
+  'Schmen',
+  2025 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(typeof jonas[i]);
+  types.push(typeof jonas[i]);
+
+  if (typeof jonas[i] !== 'string') continue;
+  console.log('18', jonas[i], typeof jonas[i]);
+}
+// console.log("17", types);
+
+const years = [1991, 1992, 1993, 1994, 1995];
+const newArray = [];
+
+for (let i = 0; i < years.length; i++) {
+  newArray.push(2027 - years[i]);
+}
+
+console.log(newArray);
